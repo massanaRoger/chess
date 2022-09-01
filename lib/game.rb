d8e0fdab39d388d5740c2e56cd0e8953227079d6
Roger Massana <rogermassana2002@gmail.com>
2022-09-01 12:01:52 +0200

    Add pawn rows to game board

--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require_relative './pieces'
+
 # Main class for the game
 class Game
   attr_accessor :board
@@ -9,6 +11,17 @@ class Game
   end
 
   def create_board
-    Array.new(8) { Array.new(8, nil) }
+    arr = Array.new(8) { Array.new(8, nil) }
+    arr[1] = pawn_row(1)
+    arr[6] = pawn_row(6)
+    arr
+  end
+
+  def pawn_row(row)
+    arr = Array.new(8, nil)
+    arr.map.with_index { |_el, index| Pawn.new(row, index) }
   end
 end
+
+game = Game.new
+p game.board
